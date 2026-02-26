06cffdffd5f2d41bb4fa2ff0c71e4927daab3fb7
Zdeněk Ulrych <zdenek.ulrych@outlook.com>
2026-02-26 14:41:50 +0100

    Přidejte soubory projektu.

---
+++ Obdelnik/Models/Obdelnik1.cs
@@ -0,0 +1,33 @@
+﻿namespace Obdelnik.Models
+{
+	public class Obdelnik1
+	{
+		public int StranaA { get; set; }
+		public int StranaB { get; set; }
+		public int Obvod { get; private set; }
+		public int Obsah { get; private set; }
+		public double Uhlopricka { get; private set; }
+
+		private void SpoctoObsah()
+		{
+			Obsah = StranaA * StranaB;
+		}
+
+		private void SpocitejObvod()
+		{
+			Obvod = 2 * (StranaA + StranaB);
+		}
+
+		private void SpocitejUhlopricku()
+		{
+			Uhlopricka = Math.Sqrt(StranaA * StranaA + Math.Pow(StranaB, 2));
+		}
+
+		public void SpocitejVse()
+		{
+			SpoctoObsah();
+			SpocitejObvod();
+			SpocitejUhlopricku();
+		}
+	}
+}
---
+++ Obdelnik/Models/Obdelnik2.cs
@@ -0,0 +1,14 @@
+﻿namespace Obdelnik.Models
+{
+	public class Obdelnik2
+	{
+		private int stranaA;
+		private int stranaB;
+		private int NormalizujStranu(int strana) => Math.Max(0, strana);
+		public int StranaA { get => stranaA; set => stranaA = NormalizujStranu(value); }
+		public int StranaB { get => stranaB; set => stranaB = NormalizujStranu(value); }
+		public int Obvod => 2 * (StranaA + StranaB);
+		public int Obsah => StranaA * StranaB;
+		public double Uhlopricka => Math.Sqrt(StranaA * StranaA + Math.Pow(StranaB, 2));
+	}
+}
